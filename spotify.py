af88f6722f39b6670c33ef4c50e3a317587b2990
Joey Donovan <josmdonovan@umass.edu>
2020-11-17 21:17:06 -0500

    added auth flow

--- spotify.py
+++ spotify.py
@@ -4,5 +4,7 @@ import spotipy.util as util
 CLIENT_ID = "b665f59008ab415c927afbc0f00b44b1"
 CLIENT_SECRET = "84f8a61e253a4a0db91a1f50e8a0a9f1"
 
+token = util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+cache_token = token.get_access_token()
+sp = spotipy.Spotify(cache_token)
 
-#hello
